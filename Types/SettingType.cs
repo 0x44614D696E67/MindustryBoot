69c930e09f1836bf605e0be110213a095258bb4e
DM <Hello-Python@outlook.com>
2026-03-07 16:42:00 +0800

    添加了ItemShow

--- Types/SettingType.cs
+++ Types/SettingType.cs
@@ -1,9 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Windows.UI;
 
 namespace MindustryBoot.Types;
 
 internal class SettingType
 {
+    #region Theme
+    public bool IsAutoDarkMode { get; set; } = true;
+    public bool IsDarkMode { get; set; } = false;
+    public bool IsAutoThemeColor { get; set; } = true;
+    public Color ThemeColor { get; set; } = Colors.Transparent;
+    public BackdropType BackgroundType { get; set; } = BackdropType.Mica;
+    #endregion
 }
